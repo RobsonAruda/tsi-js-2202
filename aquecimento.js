d1605d2b178a0a5587d1afb0fc0af750836e4308
ROBSON ARRUDA LIMA <robson.alima1@senacsp.edu.br>
2022-08-26 22:12:37 -0300

    Condicionais, repetição Operadores e tenário

---
+++ aquecimento.js
@@ -0,0 +1,79 @@
+/*
+for
+for (var i = 0; i <= 9; i++) {
+    
+    console.log('Numero da linha: ' + i);
+}
+
+while
+let i = 1;
+while (let = i <= 10) {
+     console.log('Numero da linha while ' +i) 
+     i++;
+     }
+let cont = 0   
+
+do while
+let  i = 1;
+do {
+    console.log('Robson ' + i);
+    i++;
+  } while (i <=10);
+
+
+ let a = 2
+ let b = 1
+    if (a > b ) {
+    console.log ( a + ' é maior que ' + b);
+    }
+    else {
+        console.log (b + ' é maior que' + a);
+    }
+
+let i = 'oi';
+switch ( i) {
+    case 1:
+        console.log ('Segunda');
+        break;
+    case 2:
+        console.log ('Terça');
+        break;
+    case 3:
+        console.log ('Quarta');
+        break;
+    case 4:
+        console.log ('Quinta');
+        break;
+    case 5:
+        console.log ('Sexta');
+        break;
+    case 6:
+        console.log ('Sabado');
+        break;
+    case 7:
+        console.log ('Domigo');
+        break;
+    default :
+        console.log ('Não é valido');
+        break;
+    
+}
+
+
+let x = 4;
+let y = 2;
+
+let resultado = x > y ? 'X é maior que Y' : 'X não é maior que y';
+console.log(resultado); 
+
+//coalesente
+let genero = prompt("Entre com M ou F");
+
+let saida = genero == 'M' ? 'Masculino' : 'Feminino';
+
+console.log(saida)
+
+let coisa ;
+let variavel = coisa ?? 'Não há';
+console.log(variavel)
+*/
