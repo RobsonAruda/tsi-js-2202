461983579bd77c927be420ac838baa22369a8b2c
ROBSON ARRUDA LIMA <robson.alima1@senacsp.edu.br>
2022-09-09 22:14:26 -0300

    Dom e eventos

---
+++ DOM/ex2.js
@@ -0,0 +1,20 @@
+let btnCalcular = document.getElementById('calcular');
+
+btnCalcular.addEventListener('Click', function (evento){
+    evento.preventDefault();
+    console.log(evento);
+    //obter os valores de serviço e prazo
+});
+
+
+
+
+/*
+btnCalcular.addEventListener('Click', executa ());
+
+function executa(evento) {
+    console.log(evento);
+}
+
+console.log(btnCalcular);
+*/
